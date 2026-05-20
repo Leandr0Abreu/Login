5f55f969852e2587663056457fdbe582bb28d778
Leandr0Abreu <leandro.gabriel.abrel.silva@gmail.com>
2026-05-20 14:05:05 -0300

    Implement mousemove rotation effect for container
    Add mousemove event listener to rotate container based on mouse position.

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.addEventListener("mousemove", (e) => {
     background.style.transform = `translate(${x}px, ${y}px)`;
 
 });
+
+const container = document.querySelector(".container");
+
+document.addEventListener("mousemove", (e) => {
+
+    const x = (window.innerWidth / 2 - e.clientX) / 25;
+
+    const y = (window.innerHeight / 2 - e.clientY) / 25;
+
+    container.style.transform =
+        `rotateY(${-x}deg) rotateX(${y}deg)`;
+
+});
